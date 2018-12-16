be3ff5816ae0ba6e0389596d192ad4c80287f4bf
ydl1998 <45841305+ydl1998@users.noreply.github.com>
2018-12-16 17:21:47 +0800

    解析影片信息

--- src/edu/csuft/spider/App.java
+++ src/edu/csuft/spider/App.java
@@ -1,6 +1,7 @@
 package edu.csuft.spider;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -23,12 +24,26 @@ public class App {
 		//使用 JSOUP抓取文档
 		try {
 			Document doc = Jsoup.connect(url).get();
-			Elements es = doc.select("img");
+			Elements es = doc.select(".grid_view .item");
 //			Elements es = doc.select("img[src]");
+//			Elements es = doc.select("img");
 			System.out.println(es.size());
+			
+			//创建
+			ArrayList<Film> list = new ArrayList<>();
 			for (Element e :es) {
+				Film f = new Film();
+				//每一部影片
+				Element t = e.select(".title").first();
+				String num = e.select(".star span").last().text();
 //				System.out.println(e.attr("src"));
-				System.out.println(e);
+//				System.out.println(e);
+				System.out.println(t.text() + "," + num);
+				
+//				f.id
+//				f.title
+				list.add(f);
+				
 			}
 			
 //			String title = doc.title();
---
+++ src/edu/csuft/spider/Film.java
@@ -0,0 +1,35 @@
+package edu.csuft.spider;
+
+public class Film {
+	/**
+	 * 影片名称
+	 * 
+	 * @author machenike
+	 */
+	String title;
+	/**
+	 * 相关信息
+	 */
+	String info;
+	/**
+	 * 评分
+	 */
+	String rating;
+	/**
+	 * 评分人数
+	 */
+	int num;
+	/**
+	 * 影片排名
+	 */
+	int id;
+	/**
+	 * 海报
+	 */
+	String poster;
+	/**
+	 * 短评
+	 */
+	String qoute;
+
+}
